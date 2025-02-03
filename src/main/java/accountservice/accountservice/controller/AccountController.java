045c4e8e1862620617d780b8b0a162b8ff5c3ae9
datnq2 <datnq2@vnpay.vn>
2025-02-03 08:57:35 +0700

    feat: api send mail after create account

--- src/main/java/accountservice/accountservice/controller/AccountController.java
+++ src/main/java/accountservice/accountservice/controller/AccountController.java
@@ -4,7 +4,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import accountservice.accountservice.client.NotificationService;
 import accountservice.accountservice.client.StatisticService;
+import accountservice.accountservice.model.MessageDTO;
 import accountservice.accountservice.model.StatisticDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,36 +29,45 @@ public class AccountController {
     private AccountService accountService;
     @Autowired
     private StatisticService statisticService;
-    
+    @Autowired
+    private NotificationService notificationService;
+
     // add new
     @PostMapping("/account")
     public AccountDTO addAccount(@RequestBody AccountDTO accountDTO) {
-	accountService.add(accountDTO);
+        accountService.add(accountDTO);
 
-    statisticService.add(new StatisticDTO("Account " + accountDTO.getUsername() + " is created", new Date()));
-	return accountDTO;
+        statisticService.add(new StatisticDTO("Account " + accountDTO.getUsername() + " is created", new Date()));
+        MessageDTO messageDTO = new MessageDTO();
+        messageDTO.setFrom("dbconsultant9@gmail.com");
+        messageDTO.setTo(accountDTO.getUsername());
+        messageDTO.setToName(accountDTO.getName());
+        messageDTO.setSubject("Heh guys 2025");
+        messageDTO.setContent("Here comes 2025");
+        notificationService.sendNotification(messageDTO);
+        return accountDTO;
     }
 
     // get all
     @GetMapping("/accounts")
     public List<AccountDTO> getAll() {
-	return accountService.getAll();
+        return accountService.getAll();
     }
-    
+
     @GetMapping("/account/{id}")
     public ResponseEntity<AccountDTO> get(@PathVariable(name = "id") Long id) {
-	return Optional.of(new ResponseEntity<AccountDTO>(accountService.getOne(id), HttpStatus.OK))
-		.orElse(new ResponseEntity<AccountDTO>(HttpStatus.NOT_FOUND));
+        return Optional.of(new ResponseEntity<AccountDTO>(accountService.getOne(id), HttpStatus.OK))
+                .orElse(new ResponseEntity<AccountDTO>(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping("/account/{id}")
     public void delete(@PathVariable(name = "id") Long id) {
-	accountService.delete(id);
+        accountService.delete(id);
     }
 
     @PutMapping("/account")
     public void update(@RequestBody AccountDTO accountDTO) {
-	accountService.update(accountDTO);
+        accountService.update(accountDTO);
     }
 
 }
