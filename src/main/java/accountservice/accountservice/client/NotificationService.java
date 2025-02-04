528d343f60760f427ed341ce32d19f349d9aec2c
dat02092k1 <ngodat02092001@gmail.com>
2025-02-05 00:03:39 +0700

    feat: add fallback

--- src/main/java/accountservice/accountservice/client/NotificationService.java
+++ src/main/java/accountservice/accountservice/client/NotificationService.java
@@ -1,12 +1,26 @@
 package accountservice.accountservice.client;
 
 import accountservice.accountservice.model.MessageDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "notification-service", url = "http://localhost:9083")
+@FeignClient(name = "notification-service", url = "http://localhost:9083", fallback = NotificationServiceFallback.class)
 public interface NotificationService {
     @PostMapping("/send-notification")
     void sendNotification(@RequestBody MessageDTO messageDTO);
+}
+
+@Component
+class NotificationServiceFallback implements NotificationService {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void sendNotification(MessageDTO messageDTO) {
+        // fallback implementation
+        logger.error("Notification service is not available or slow");
+    }
 }
--- src/main/java/accountservice/accountservice/client/StatisticService.java
+++ src/main/java/accountservice/accountservice/client/StatisticService.java
@@ -1,12 +1,25 @@
 package accountservice.accountservice.client;
 
 import accountservice.accountservice.model.StatisticDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "statistic-service", url = "http://localhost:9082")
+@FeignClient(name = "statistic-service", url = "http://localhost:9082", fallback = StatisticServiceFallback.class)
 public interface StatisticService {
     @PostMapping("/statistic")
     void add(@RequestBody StatisticDTO statisticDTO);
+}
+
+@Component
+class StatisticServiceFallback implements StatisticService {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Override
+    public void add(StatisticDTO statisticDTO) {
+        // fallback implementation
+        logger.error("Statistic service is not available or slow");
+    }
 }
